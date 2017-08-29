132df6d760314b53c9fadb894c38423f314bce65
James Word <30912590+wordjames@users.noreply.github.com>
2017-08-28 20:09:42 -0700

    Chapter 4 Exercise

---
+++ chapter_4/greetings.rb
@@ -0,0 +1,7 @@
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'Middle name?'
+middle_name = gets.chomp
+puts 'Last name?'
+last_name = gets.chomp
+puts 'Hello there ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
