46738a2775ba3264a87aec37ac0ed57e4f1c0fc8
James Word <30912590+wordjames@users.noreply.github.com>
2017-08-28 19:53:53 -0700

    Chapter 1 Exercise

---
+++ chapter_1/arithmetic.rb
@@ -0,0 +1,8 @@
+puts "Hours in a year:"
+puts 24 * 365
+puts "Minutes in a decade:"
+puts 60 * 24 * 365 * 10
+puts "My age in seconds:" #23 at the time, 62 days from bday)
+puts (60 * 60 * 24 * 365 * 23) + (60 * 60 * 24 * 62) + (60 * 60 * 9) + (60 * 34) 
+puts "The author's age:"
+puts 1025000000.0 / (60 * 60 * 24 * 365)
