74e490b1fc8023abf9ea1072f8470ad990e9b05f
James Word <30912590+wordjames@users.noreply.github.com>
2017-08-28 20:14:38 -0700

    Create fav_number.rb

---
+++ chapter_4/fav_number.rb
@@ -0,0 +1,4 @@
+puts 'Good evening! What is your favorite number?'
+num = gets.chomp
+better = num.to_i + 1
+puts num + ' is alright, but ' + better.to_s + ' is a bigger and better favorite number!'
