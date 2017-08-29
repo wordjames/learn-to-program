8975178c9d0e19643fc446de3359ca468fc791e8
James Word <jameslword@gmail.com>
2017-08-28 21:03:43 -0700

    added table_of_contents.rb

---
+++ chapter_7/table_of_contents.rb
@@ -0,0 +1,9 @@
+Chapters = [
+  'Chapter 1:  Numbers                                page 1',
+  'Chapter 2:  Letters                               page 72',
+  'Chapter 3:  Variables                            page 118']
+lineWidth = 60     
+
+title = 'Table of contents'
+puts title.center lineWidth
+puts Chapters
